1982a0a983a2de98b2e159819d06872c6dcaf579
davidmgaviria <davidmgaviria123@gmail.com>
2021-07-16 00:00:22 -0400

    First commit

---
+++ common/defines/wsv_defines.lua
@@ -0,0 +1,32 @@
+
+NDefines.NGame.START_DATE = "1910.1.1.12"						
+NDefines.NGame.END_DATE = "1950.1.1.1"
+
+NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 1000			--Max army experience a country can store
+NDefines.NMilitary.MAX_NAVY_EXPERIENCE = 1000			--Max navy experience a country can store
+NDefines.NMilitary.MAX_AIR_EXPERIENCE = 1000				--Max air experience a country can store
+
+NDefines.NMilitary.EXILE_ORG = 0.2					--they need to be able to move
+
+NDefines.NNavy.NAVAL_SUPREMACY_CAN_INVADE = 0.75
+
+NDefines.NProduction.BASE_FACTORY_SPEED = 3.75 				-- Base civ factory speed multiplier (how much hoi3 style IC each factory gives).
+NDefines.NProduction.BASE_FACTORY_SPEED_MIL = 3.4 				-- Base mil factory speed multiplier (how much hoi3 style IC each factory gives).
+
+NDefines.NCountry.DEFAULT_WAR_SUPPORT = 0.2
+NDefines.NCountry.BASE_STABILITY_WAR_FACTOR = -0.2
+
+--war stuff
+NDefines.NMilitary.PLAN_EXECUTE_BALANCED_LIMIT = 5				-- When looking for an attach target, this score limit is required in the battle plan to consider province for attack
+--NDefines.NMilitary.PLAN_NEIGHBORING_ENEMY_PROVINCE_FACTOR = 0.6	-- When calculating the importance of provinces, it takes number of enemy provinces into account, factored by this
+--NDefines.NMilitary.PLAN_PROVINCE_BASE_IMPORTANCE = 1.0			-- Used when calculating the calue of front and defense area provinces for the battle plan system
+	
+--NDefines.NMilitary.PLAN_PROVINCE_LOW_VP_IMPORTANCE_FRONT = 2.0    -- Used when calculating the calue of fronts in the battle plan system
+--NDefines.NMilitary.PLAN_PROVINCE_MEDIUM_VP_IMPORTANCE_FRONT = 7.5 -- Used when calculating the calue of fronts in the battle plan system
+--NDefines.NMilitary.PLAN_PROVINCE_HIGH_VP_IMPORTANCE_FRONT = 15.0  -- Used when calculating the calue of fronts in the battle plan system
+
+--resistance
+--NDefines.NCountry.RESISTANCE_DECAY_WHEN_NO_GROWTH = 0.005   		--originally 0.005
+--NDefines.NCountry.SUPPRESSION_NEEDED_BY_RESISTANCE_POINT = 0.75  		--originally 0.75
+
+
---
+++ descriptor.mod
@@ -0,0 +1,6 @@
+version="1.9"
+tags={
+	"Alternative History"
+}
+name="Featherstone Tester"
+supported_version="1.10.*"
